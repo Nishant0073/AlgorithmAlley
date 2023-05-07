7cbd0926032dec9442ee4ccf7e062da674d5cb3b
Nishant Shingate <nishantks121@gmail.com>
2023-05-07 15:34:30 +0530

    1971. Find if Path Exists in Graph

---
+++ GRAPH/1971. Find if Path Exists in Graph.cpp
@@ -0,0 +1,66 @@
+class UnionFind
+{
+    private:
+    vector<int> v;
+
+    public:
+    UnionFind(int n)
+    {
+        for(int i=0;i<n;i++)
+            v.push_back(i);
+    }
+
+    void unionf(int x,int y)
+    {
+        int xx = find(x);
+        int yy = find(y);
+
+        if(xx!=yy)
+            v[xx] = yy;
+    }
+
+    int  find(int x)
+    {
+        int tmp = x;
+        while(tmp!=v[tmp])
+            tmp = v[tmp];
+        
+        //path compression
+        while(x!=tmp)
+        {
+            int next = v[x];
+            v[x] = tmp;
+            x = next;
+        }
+
+        return x;
+    }
+    
+};
+
+class Solution {
+public:
+    /*
+    0 1 2
+    0 1 2
+
+    0 1 2
+    0 0 0
+    
+    */
+
+    bool validPath(int n, vector<vector<int>>& edges, int source, int dest)
+    {
+
+        UnionFind* u = new UnionFind(n);
+
+         for(int i=0;i<edges.size();i++)
+             u->unionf(edges[i][0],edges[i][1]);
+
+        int x = u->find(source);
+        int y = u->find(dest);
+        
+        cout<<x<<" "<<y<<endl;
+        return x==y;
+    }       
+};
